72e755c8843063ea37f763788fdb1357fb70d4cd
César Guzmán <cesar.guzman2000@sitiouno.com>
2024-02-05 20:35:58 -0400

    Add extra config

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+const Config = {
+  exitRoutes: ['CreatePayment'],
+  API_URL: 'https://payments.pre-bnvo.com/api/v1/',
+  IDENTIFIER: '2fb2b71c-4a1d-4c56-a28f-95552398799a',
+}
+
+export default Config
