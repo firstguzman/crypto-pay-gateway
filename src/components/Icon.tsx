769b0098922575b29a9ee83d8993513e9738f335
César Guzmán <cesar.guzman2000@sitiouno.com>
2024-02-07 10:16:49 -0400

    Add Icon component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,100 @@
+import * as React from 'react'
+import { ComponentType } from 'react'
+import {
+  Image,
+  ImageStyle,
+  StyleProp,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+  ViewProps,
+  ViewStyle,
+} from 'react-native'
+
+export type IconTypes = keyof typeof iconRegistry
+
+interface IconProps extends TouchableOpacityProps {
+  /**
+   * The name of the icon
+   */
+  icon: IconTypes
+
+  /**
+   * An optional tint color for the icon
+   */
+  color?: string
+
+  /**
+   * An optional size for the icon. If not provided, the icon will be sized to the icon's resolution.
+   */
+  size?: number
+
+  /**
+   * Style overrides for the icon image
+   */
+  style?: StyleProp<ImageStyle>
+
+  /**
+   * Style overrides for the icon container
+   */
+  containerStyle?: StyleProp<ViewStyle>
+
+  /**
+   * An optional function to be called when the icon is pressed
+   */
+  onPress?: TouchableOpacityProps['onPress']
+}
+
+/**
+ * A component to render a registered icon.
+ * It is wrapped in a <TouchableOpacity /> if `onPress` is provided, otherwise a <View />.
+ *
+ */
+export function Icon(props: IconProps) {
+  const {
+    icon,
+    color,
+    size,
+    style: $imageStyleOverride,
+    containerStyle: $containerStyleOverride,
+    ...WrapperProps
+  } = props
+
+  const isPressable = !!WrapperProps.onPress
+  const Wrapper = (
+    WrapperProps?.onPress ? TouchableOpacity : View
+  ) as ComponentType<TouchableOpacityProps | ViewProps>
+
+  const $imageStyle: StyleProp<ImageStyle> = [
+    $imageStyleBase,
+    color !== undefined && { tintColor: color },
+    size !== undefined && { width: size, height: size },
+    $imageStyleOverride,
+  ]
+
+  return (
+    <Wrapper
+      accessibilityRole={isPressable ? 'imagebutton' : undefined}
+      {...WrapperProps}
+      style={$containerStyleOverride}
+    >
+      <Image style={$imageStyle} source={iconRegistry[icon]} />
+    </Wrapper>
+  )
+}
+
+export const iconRegistry = {
+  arrowDown: require('../../assets/icons/arrowDown.png'),
+  arrowRight: require('../../assets/icons/arrowRight.png'),
+  copy: require('../../assets/icons/copy.png'),
+  infoCircle: require('../../assets/icons/infoCircle.png'),
+  search: require('../../assets/icons/search.png'),
+  tickCircle: require('../../assets/icons/tickCircle.png'),
+  timer: require('../../assets/icons/timer.png'),
+  verify: require('../../assets/icons/verify.png'),
+  warning: require('../../assets/icons/warning.png'),
+}
+
+const $imageStyleBase: ImageStyle = {
+  resizeMode: 'contain',
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './Button'
 export * from './Dropdown'
+export * from './Icon'
 export * from './Screen'
 export * from './Text'
 export * from './TextField'
