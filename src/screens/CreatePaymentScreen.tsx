2c2536082e39ef0dd9a4a815b6f4253cc83efea6
César Guzmán <cesar.guzman2000@sitiouno.com>
2024-02-06 20:47:08 -0400

    Add UI components to Screen

--- src/screens/CreatePaymentScreen.tsx
+++ src/screens/CreatePaymentScreen.tsx
@@ -1,11 +1,83 @@
-import { FC } from 'react'
-import { Text } from 'react-native'
-import { Screen } from '../components'
+import { StackActions } from '@react-navigation/native'
+import { FC, useState } from 'react'
+import { ViewStyle } from 'react-native'
+import { Button, Dropdown, Screen, TextField } from '../components'
 
-export const CreatePaymentScreen: FC<any> = () => {
+import { AppStackScreenProps } from '../navigators'
+import { spacing } from '../theme'
+
+interface CreatePaymentScreenProps
+  extends AppStackScreenProps<'CreatePayment'> {}
+
+export const CreatePaymentScreen: FC<CreatePaymentScreenProps> = ({
+  navigation,
+}) => {
+  const [amount, setAmount] = useState<string>()
+  const [notes, setNotes] = useState<string>()
+  const [currency, setCurrency] = useState<string>()
+
+  const [items, setItems] = useState([
+    { label: 'Apple', value: 'apple' },
+    { label: 'Banana', value: 'banana' },
+  ])
+
+  const onSuccessAction = () => {
+    navigation.dispatch(StackActions.replace('OrderSummary'))
+  }
+
   return (
-    <Screen preset="fixed" safeAreaEdges={['bottom']}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Screen preset="auto" safeAreaEdges={['bottom']} style={$container}>
+      <TextField
+        value={amount}
+        onChangeText={(value) => setAmount(value)}
+        containerStyle={$textField}
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardType="numeric"
+        label={'Importe a pagar'}
+        placeholder={'Añade importe a pagar'}
+        // helper={error}
+        // status={error ? "error" : undefined}
+      />
+
+      <Dropdown
+        items={items}
+        value={currency}
+        onValueChange={setCurrency}
+        label={'Seleccionar moneda'}
+        placeholder={'Selecciona una moneda'}
+        searchPlaceholder={'Buscar'}
+        dropdownWrapperStyle={$textField}
+      />
+
+      <TextField
+        value={notes}
+        onChangeText={(value) => setNotes(value)}
+        containerStyle={$textField}
+        autoCapitalize="none"
+        autoCorrect={false}
+        label={'Concepto'}
+        placeholder={'Añade descripción del pago'}
+        // helper={error}
+        // status={error ? "error" : undefined}
+        // onSubmitEditing={() => authPasswordInput.current?.focus()}
+      />
+
+      <Button
+        onPress={onSuccessAction}
+        onLongPress={onSuccessAction}
+        text={'Continuar'}
+        disabled={!amount || !currency || !notes}
+      />
     </Screen>
   )
+}
+
+const $container: ViewStyle = {
+  paddingHorizontal: spacing.md,
+  paddingTop: spacing.lg,
+}
+
+const $textField: ViewStyle = {
+  marginBottom: spacing.xl,
 }
