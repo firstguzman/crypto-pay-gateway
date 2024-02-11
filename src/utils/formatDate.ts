e20e5f6a279156ef7d5f10a249f670b19475427a
César Guzmán <cesar.guzman2000@sitiouno.com>
2024-02-11 13:35:04 -0400

    Fix Android error

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,14 +1,15 @@
 import { NativeModules, Platform } from 'react-native'
 
-const LOCALE = Platform.select({
-  ios:
-    NativeModules.SettingsManager.settings.AppleLocale ||
-    NativeModules.SettingsManager.settings.AppleLanguages[0],
-  android: NativeModules.I18nManager.localeIdentifier,
-})
-
 export const formatDate = (dateString: string) => {
   const inputDate = new Date(dateString)
+  let LOCALE = ''
+  if (Platform.OS === 'android') {
+    LOCALE = NativeModules.I18nManager.localeIdentifier
+  } else {
+    LOCALE =
+      NativeModules.SettingsManager.settings.AppleLocale ||
+      NativeModules.SettingsManager.settings.AppleLanguages[0]
+  }
 
   const formattedDate = `${inputDate.toLocaleDateString(
     LOCALE.replace('_', '-'),
